5fc5cf9e50045bcf7015eb7a6376e815c3c0431c
DANIEL2DAM <dmarrom577@g.educaand.es>
2023-01-23 12:26:07 +0100

    instituto

--- app/src/main/java/www/iesmurgi/flipdaniel/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/flipdaniel/MainActivity.kt
@@ -2,10 +2,87 @@ package www.iesmurgi.flipdaniel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import org.w3c.dom.Text
+import www.iesmurgi.flipdaniel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sbEjeX: SeekBar
+    private lateinit var sbEjeY: SeekBar
+    private lateinit var sbTrama: SeekBar
+    private lateinit var tvEjeX: TextView
+    private lateinit var tvEjeY: TextView
+    private lateinit var tvTramas: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sbEjeX = binding.sbEjeX
+        sbEjeY = binding.sbEjeY
+        sbTrama = binding.sbTrama
+
+        sbEjeX.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tvEjeX = binding.tvEjeX
+                //updateXTiles(seekBar?.getProgress()!!)
+                tvEjeX.text=progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        sbEjeY.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tvEjeY = binding.tvEjeY
+                //updateXTiles(seekBar?.getProgress()!!)
+                tvEjeY.text=progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        sbTrama.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tvTramas = binding.tvTramas
+                //updateXTiles(seekBar?.getProgress()!!)
+                tvTramas.text=progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+
     }
+
+
+    fun startPlay() {}
+
+
+
+
 }
